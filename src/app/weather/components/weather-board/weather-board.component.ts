e96081bf3fcdf4945f1e95c1bf0bf84f6b93ff75
Esteban <garciawarneckee@gmail.com>
2019-01-18 19:33:20 +0100

    saving weather historic

--- src/app/weather/components/weather-board/weather-board.component.ts
+++ src/app/weather/components/weather-board/weather-board.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../../services/weather-api//weather.service';
 import { CityWeather } from '../../model/weather-dto';
 import * as moment from 'moment';
+import { WeatherStorageService } from '../../services/weather-storage/weather-storage.service';
 
 @Component({
   selector: 'app-weather-board',
@@ -12,12 +13,17 @@ export class WeatherBoardComponent implements OnInit {
 
   weathers: Array<CityWeather> = null;
 
-  constructor(private weatherService: WeatherService) { }
+  constructor(private weatherService: WeatherService, 
+    private weatherStorage: WeatherStorageService) { }
 
   ngOnInit() {
     /**Getting weathers for first time */
-    this.weatherService.getDefaultCitiesWeather(['Barcelona', 'Londres', 'Washington'])
-      .then(weathers => this.weathers = weathers);
+    this.weatherService
+    .getDefaultCitiesWeather(['Barcelona', 'Londres', 'Washington'])
+    .then(weathers => { 
+      this.weathers = weathers;
+      this.weatherStorage.bulkSave(weathers);
+    });
 
     /** Listening for new weathers for every 3 minutes */  
     this.weatherService.getCitiesWheathersInterval(3)
@@ -25,6 +31,7 @@ export class WeatherBoardComponent implements OnInit {
       const now = moment().format('LLL'); ;
       console.log(`Getting new weathers at + ${now}`);
       this.weathers = weathers;
+      this.weatherStorage.bulkSave(weathers);
     } });
   }
 
