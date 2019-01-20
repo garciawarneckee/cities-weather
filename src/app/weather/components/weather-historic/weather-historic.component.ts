7cf42b8054481af046bb5d992ae850ef4e95aabe
Esteban <garciawarneckee@gmail.com>
2019-01-20 23:27:56 +0100

    adding table of historic

--- src/app/weather/components/weather-historic/weather-historic.component.ts
+++ src/app/weather/components/weather-historic/weather-historic.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { WeatherStorageService } from '../../services/weather-storage/weather-storage.service';
 import { ActivatedRoute } from '@angular/router';
 import CityWeather from '../../model/weather';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-weather-historic',
@@ -16,7 +17,8 @@ export class WeatherHistoricComponent implements OnInit {
   constructor(private route: ActivatedRoute, private weatherStorage: WeatherStorageService) { }
 
   ngOnInit() {
-    this.historic = this.weatherStorage.getHistoricByCity(this.cityName);
+    Observable.of(this.weatherStorage.getHistoricByCity(this.cityName))
+      .subscribe(historic => this.historic = historic) ;
   }
 
 }
